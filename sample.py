623f7f333446e66c64e2e215a55c9e902b712f98
Jonah Dienye <jnhdny@gmail.com>
2011-11-07 21:21:03 +0100

    COnsolidated user data queries into one function

--- sample.py
+++ sample.py
@@ -10,41 +10,25 @@ def runQuery(query, parameters):
 	c.close()
 	return results
 
-
-def userID(email):
-	results = runQuery("select id from x_user where email = %s;", (email,))
-	return results[0][0]
-
-def availableNaira(email):
-	results = runQuery("select availablenaira from x_user where email =%s;", (email,))
-	if not results[0][0]:
-		return 0
-	else:
-		return results[0][0]
-
-def nairaBalance(email):
-	results = runQuery("select nairabalance from x_user where email =%s;", (email,))
-	if not results[0][0]:
-		return 0
-	else:
-		return results[0][0]
-
-def dollarBalance(email):
-	results = runQuery("select dollarbalance from x_user where email =%s;", (email,))
-	if not results[0][0]:
-		return 0
-	else:
-		return results[0][0]
+#Consolidate user detail queries
+def userDetails(email):
+	results = runQuery("select id, availablenaira,nairabalance,dollarbalance from x_user where email =%s;", (email,))
+	rr = {}
+	rr["id"] = results[0][0]
+	rr["availablenaira"] = results[0][1]
+	rr["nairabalance"] = results[0][2]
+	rr["dollarbalance"] = results[0][3]
+	return rr
 
 	
 def createAuction(amount,close_date):
     c = tt.cursor()
     c.execute('''insert into x_auction (dollars,close_date) values (%s,%s);''',(amount,close_date))
-    lid = c.lastrowid
+    lrid = c.lastrowid
     tt.commit()
     c.close()
-    runQuery('''create event ev%s on schedule at %s do update x_auction set status = 1 where id = %s;''',(lid,close_date,lid))
-    runQuery('''create event cb%s on schedule at %s + interval 1 minute do call checkbids(%s);''',(lid,close_date,lid))
+    runQuery('''create event ev%s on schedule at %s do update x_auction set status = 1 where id = %s;''',(lrid,close_date,lrid))
+    runQuery('''create event cb%s on schedule at %s + interval 1 minute do call checkbids(%s);''',(lrid,close_date,lrid))
 
 def auctionstatus(id):
 	results = runQuery("select status from x_auction where id = %s", (id,))
@@ -53,8 +37,7 @@ def auctionstatus(id):
 def bid(email, id, amount, rate):
     nairabalance = Decimal(nairaBalance(email))
     if  (nairabalance >= amount*rate) and auctionstatus(id) == 0:
-        runQuery("insert into x_bid (rate,dollars,status,auction_id,user_id) values (%s,%s,%s,%s,%s);",(rate,amount,0,id,userID(email)))
-
+        runQuery("insert into x_bid (rate,dollars,status,auction_id,user_id) values (%s,%s,%s,%s,%s);",(rate,amount,0,id,userDetails(email)["id"]))
 
 
 
