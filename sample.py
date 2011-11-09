5a3f22c367c9971156eab95202b4413d367deff3
Jonah Dienye <jnhdny@gmail.com>
2011-11-09 17:36:11 +0100

    Added fancy list mapping for userDetail and auctionDetail functions

--- sample.py
+++ sample.py
@@ -13,28 +13,16 @@ def runQuery(query, parameters):
 #Consolidate user detail queries
 def userDetails(email):
 	results = runQuery("select id, availablenaira,nairabalance,dollarbalance from x_user where email =%s;", (email,))
-	rr = {}
-	rr["id"] = results[0][0]
-	rr["availablenaira"] = results[0][1]
-	rr["nairabalance"] = results[0][2]
-	rr["dollarbalance"] = results[0][3]
-	return rr
+	# Courtesy http://stackoverflow.com/questions/209840/map-two-lists-into-a-dictionary-in-python
+	return dict(zip(["id", "availablenaira", "nairabalance", "dollarbalance"],results[0]))
 
 #Consolidate auction detail query
 def auctionDetails(id):
     results = runQuery("select id, dollars, status, rate, creation_date, close_date, sold from x_auction where id =%s;", (id,))
-    rr = {}
     try:
-        rr["id"] = results[0][0]
-        rr["dollars"] = results[0][1]
-        rr["status"] = results[0][2]
-        rr["rate"] = results[0][3]
-        rr["creation_date"] = results[0][4]
-        rr["close_date"] = results[0][5]
-        rr["sold"] = results[0][6]
+        return dict(zip(["id","dollars","status","rate","creation_date","close_date","sold"],results[0]))
     except:
         return 0
-    return rr
 	
 def createAuction(amount,close_date):
     c = tt.cursor()
@@ -46,6 +34,7 @@ def createAuction(amount,close_date):
     runQuery('''create event ev%s on schedule at %s do update x_auction set status = 1 where id = %s;''',(lrid,close_date,lrid))
     #The following event computes bid winners and updates their account balances 1 minute after the bid ends
     runQuery('''create event cb%s on schedule at %s + interval 1 minute do call checkbids(%s);''',(lrid,close_date,lrid))
+    return 1
 
 def bid(email, id, amount, rate):
     availablenaira = Decimal(userDetails(email)["availablenaira"])
@@ -70,3 +59,4 @@ def login(email,password):
 def nairaReload(email,amount):
     runQuery('''update x_user set availablenaira=availablenaira+%s, nairabalance=nairabalance+%s where email=%s''', (amount,amount,email))
 
+    
